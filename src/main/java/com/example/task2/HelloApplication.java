56847de6f9785cdf67966b2a4fa887f08fbbc991
23yulia03 <kozlovskayayulia23@gmail.com>
2024-12-26 05:29:53 +0300

    task7-Наблюдатель

--- src/main/java/com/example/task2/HelloApplication.java
+++ src/main/java/com/example/task2/HelloApplication.java
@@ -2,22 +2,21 @@ package com.example.task2;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class HelloApplication extends Application {
+
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 800, 600);  // Увеличим размер окна для работы с фигурами
-        stage.setTitle("Figure Drawer");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+        primaryStage.setTitle("Time Server");
+        primaryStage.setScene(new Scene(root, 600, 500));
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
